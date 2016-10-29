73aa286834c7466d2a9be6169f3ac18712c947a5
José Ballester <jose@ballester.me>
2016-10-29 18:03:09 +0200

    Fase I prova 1

---
+++ faseI.m
@@ -0,0 +1,18 @@
+function [VB2, z] = SimplexFaseI (A, b)
+    m = length(A(:,1));
+    n = length(A);
+    Id = zeros(m);
+    A = [A, Id];
+    VB = n+1:n+m;
+    VN = 1:n;
+    c = [zeros(1, n), ones(1, m)];
+    B = A(:, VB);
+    invB = inv(B);
+    xB = invB*b;
+    z = c(VB)*xB;
+    res = 0;
+    while(res == 0)
+        [VB, VN, xB, z, res] = ItSimplexFaseII (A, VB, VN, xB, z, c);
+    end
+    z
+end
